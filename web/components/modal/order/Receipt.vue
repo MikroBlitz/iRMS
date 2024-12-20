ca0259efcc994027e86f9fb97d1f6660570608cb
maktezi <mlcnierras@gmail.com>
2024-12-20 12:55:39 +0800

    refactor: create a reusable component for receipt

--- web/components/modal/order/Receipt.vue
+++ web/components/modal/order/Receipt.vue
@@ -1,160 +1,20 @@
 <template>
-    <div id="receipt" class="flex items-center justify-center">
-        <div
-            class="bg-white p-6 rounded shadow-md w-[350px] text-xs max-w-md font-mono border border-gray-300"
-        >
-            <!-- Store Information (Header) -->
-            <div class="text-center mb-2 text-black">
-                <h1 class="text-lg font-bold">
-                    {{ storeName }}
-                </h1>
-                <p>{{ storeAddress }}</p>
-                <p>VAT Registered TIN: {{ vatNumber }}</p>
-                <p>MIN: {{ minNumber }}</p>
-                <p>SN: {{ serialNumber }}</p>
-            </div>
-
-            <!-- POS & Customer Information -->
-            <div class="flex justify-between text-black">
-                <div>
-                    <p>POS {{ posId }}</p>
-                    <p>Guest</p>
-                </div>
-                <div>
-                    <p>{{ auth.user.name }}</p>
-                </div>
-            </div>
-
-            <!-- Itemized List -->
-            <table class="w-full mb-2 text-xs text-black">
-                <thead>
-                    <tr class="border-b border-gray-300">
-                        <th class="text-left py-1">Item</th>
-                        <th class="text-right py-1">Qty</th>
-                        <th class="text-right py-1">Price</th>
-                        <th class="text-right py-1">Total</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr
-                        v-for="i in orderItems"
-                        :key="i.id"
-                        class="border-b border-gray-300"
-                    >
-                        <td class="py-1">
-                            <span class="break-words overflow-auto">{{
-                                i.product.name
-                            }}</span>
-                        </td>
-                        <td class="text-right px-1">
-                            {{ i.qty }}
-                        </td>
-                        <td class="text-right px-1">
-                            {{ currencyFormat(i.total_amount / i.qty) }}
-                        </td>
-                        <td class="text-right px-1">
-                            {{ currencyFormat(i.total_amount) }}
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-
-            <!-- Subtotals and Discounts -->
-            <div class="mb-4 text-black">
-                <div class="flex justify-between">
-                    <p>Subtotal:</p>
-                    <p>{{ currencyFormat(subTotal) }}</p>
-                </div>
-                <div class="flex justify-between">
-                    <p>Less 10% discount:</p>
-                    <p>{{ currencyFormat(promotionAmount) }}</p>
-                </div>
-                <div class="flex justify-between">
-                    <p>Tax 12%:</p>
-                    <p>{{ currencyFormat(Number(totalTax)) }}</p>
-                </div>
-            </div>
-
-            <!-- Total Amount and Payment -->
-            <div class="mb-4 font-bold text-black">
-                <div class="flex justify-between">
-                    <p>Total:</p>
-                    <p>
-                        {{ currencyFormat(totalAmount) }}
-                    </p>
-                </div>
-                <div class="flex justify-between">
-                    <p>Payment Received:</p>
-                    <p>{{ currencyFormat(cashTendered) }}</p>
-                </div>
-                <div class="flex justify-between">
-                    <p>Change:</p>
-                    <p>{{ currencyFormat(change) }}</p>
-                </div>
-            </div>
-
-            <div class="mb-4 text-black">
-                <p class="text-center">Sales Information</p>
-                <div class="flex justify-between">
-                    <p>VAT Sales:</p>
-                    <p>{{ formatPrice(subTotal) }}</p>
-                </div>
-                <div class="flex justify-between">
-                    <p>Non-VAT Sales:</p>
-                    <p>0.00</p>
-                </div>
-                <div class="flex justify-between">
-                    <p>Zero-Rated Sales:</p>
-                    <p>0.00</p>
-                </div>
-                <div class="flex justify-between">
-                    <p>Total VAT:</p>
-                    <p>{{ formatPrice(Number(totalTax)) }}</p>
-                </div>
-            </div>
-
-            <!-- Transaction Details -->
-            <div class="mb-2 text-black">
-                <p>Transaction #: {{ transactionNumber }}</p>
-                <p>Date/Time: {{ toBasicDateTime(date) }}</p>
-            </div>
-
-            <!-- Footer Message -->
-            <div class="border-t text-center pt-4 text-black">
-                <p>FOR ONGOING PROMOS</p>
-                <p>visit: {{ website }}</p>
-                <p class="font-bold">Thank you, please come again!</p>
-            </div>
-
-            <!-- Customer Signature Area -->
-            <div class="space-y-1 pt-4 text-black">
-                <div class="border-b">
-                    <p>Customer: {{ customerGuest }}</p>
-                </div>
-                <div class="border-b">
-                    <p>Address:</p>
-                </div>
-                <div class="border-b">
-                    <p>Signature:</p>
-                </div>
-            </div>
-
-            <!-- Print Button -->
-            <div class="mt-6 text-center">
-                <Button class="px-4 py-2" @click="printReceipt(customerGuest)">
-                    Print Receipt
-                </Button>
-            </div>
-        </div>
-    </div>
+    <PosViewReceipt
+        :id="id"
+        :cash-tendered="cashTendered"
+        :change="change"
+        :customer-guest="customerGuest"
+        :date="date"
+        :order-items="orderItems"
+        :promotion-amount="promotionAmount"
+        :sub-total="subTotal"
+        :total-amount="totalAmount"
+        :total-tax="totalTax"
+        :transaction-number="transactionNumber"
+    />
 </template>
 
 <script setup lang="ts">
-import { storeName } from '~/composables/useConstant';
-import { printReceipt } from '~/utils/pos';
-
-const auth = useAuth();
-
 const props: any = defineProps({
     cashTendered: {
         default: '',
@@ -199,15 +59,6 @@ const subTotal = computed(() => {
 });
 const totalTax = computed(() => subTotal.value * taxRate);
 const promotionAmount = computed(() => subTotal.value * discountRate);
-</script>
 
-<style scoped>
-@media print {
-    button {
-        display: none;
-    }
-}
-* {
-    font-family: 'Courier New', Courier, monospace;
-}
-</style>
+console.log(promotionAmount.value);
+</script>
--- web/components/pos/Receipt.vue
+++ web/components/pos/Receipt.vue
@@ -1,167 +1,22 @@
 <template>
-    <div id="receipt" class="flex items-center justify-center">
-        <div
-            class="bg-white p-6 rounded shadow-md w-[350px] text-xs max-w-md font-mono border border-gray-300"
-        >
-            <!-- Store Information (Header) -->
-            <div class="text-center mb-2 text-black">
-                <h1 class="text-lg font-bold">
-                    {{ storeName }}
-                </h1>
-                <p>{{ storeAddress }}</p>
-                <p>VAT Registered TIN: {{ vatNumber }}</p>
-                <p>MIN: {{ minNumber }}</p>
-                <p>SN: {{ serialNumber }}</p>
-            </div>
-
-            <!-- POS & Customer Information -->
-            <div class="flex justify-between text-black">
-                <div>
-                    <p>POS {{ posId }}</p>
-                    <p>{{ customerType }}</p>
-                </div>
-                <div>
-                    <p>{{ auth.user.name }}</p>
-                </div>
-            </div>
-
-            <!-- Itemized List -->
-            <table class="w-full mb-2 text-xs text-black">
-                <thead>
-                    <tr class="border-b border-gray-300">
-                        <th class="text-left py-1">Item</th>
-                        <th class="text-right py-1">Qty</th>
-                        <th class="text-right py-1">Price</th>
-                        <th class="text-right py-1">Total</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr
-                        v-for="i in cartStore.cartItems"
-                        :key="i.id"
-                        class="border-b border-gray-300"
-                    >
-                        <td class="py-1">
-                            <span class="break-words overflow-auto">{{
-                                i.item
-                            }}</span>
-                        </td>
-                        <td class="text-right px-1">
-                            {{ i.qty }}
-                        </td>
-                        <td class="text-right px-1">
-                            {{ formatPrice(i.price) }}
-                        </td>
-                        <td class="text-right px-1">
-                            {{ formatPrice(i.amount) }}
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-
-            <!-- Subtotals and Discounts -->
-            <div class="mb-4 text-black">
-                <div class="flex justify-between">
-                    <p>Subtotal:</p>
-                    <p>{{ currencyFormat(cartStore.totalAmount) }}</p>
-                </div>
-                <div class="flex justify-between">
-                    <p>Less 10% discount:</p>
-                    <p>{{ currencyFormat(cartStore.promotionAmount) }}</p>
-                </div>
-                <div class="flex justify-between">
-                    <p>Tax 12%:</p>
-                    <p>{{ currencyFormat(cartStore.totalTax) }}</p>
-                </div>
-            </div>
-
-            <!-- Total Amount and Payment -->
-            <div class="mb-4 font-bold text-black">
-                <div class="flex justify-between">
-                    <p>Total:</p>
-                    <p>
-                        {{
-                            currencyFormat(
-                                cartStore.totalAmountWithTaxAndDiscount,
-                            )
-                        }}
-                    </p>
-                </div>
-                <div class="flex justify-between">
-                    <p>Payment Received:</p>
-                    <p>{{ currencyFormat(cashTendered) }}</p>
-                </div>
-                <div class="flex justify-between">
-                    <p>Change:</p>
-                    <p>{{ currencyFormat(change) }}</p>
-                </div>
-            </div>
-
-            <div class="mb-4 text-black">
-                <p class="text-center">Sales Information</p>
-                <div class="flex justify-between">
-                    <p>VAT Sales:</p>
-                    <p>{{ formatPrice(cartStore.totalAmount) }}</p>
-                </div>
-                <div class="flex justify-between">
-                    <p>Non-VAT Sales:</p>
-                    <p>0.00</p>
-                </div>
-                <div class="flex justify-between">
-                    <p>Zero-Rated Sales:</p>
-                    <p>0.00</p>
-                </div>
-                <div class="flex justify-between">
-                    <p>Total VAT:</p>
-                    <p>{{ formatPrice(cartStore.totalTax) }}</p>
-                </div>
-            </div>
-
-            <!-- Transaction Details -->
-            <div class="mb-2 text-black">
-                <p>Transaction #: {{ generateTransactionId }}</p>
-                <p>Date/Time: {{ transactionDate }}</p>
-            </div>
-
-            <!-- Footer Message -->
-            <div class="border-t text-center pt-4 text-black">
-                <p>FOR ONGOING PROMOS</p>
-                <p>visit: {{ website }}</p>
-                <p class="font-bold">Thank you, please come again!</p>
-            </div>
-
-            <!-- Customer Signature Area -->
-            <div class="space-y-1 pt-4 text-black">
-                <div class="border-b">
-                    <p>Customer: {{ customerName }}</p>
-                </div>
-                <div class="border-b">
-                    <p>Address: {{ customerAddress }}</p>
-                </div>
-                <div class="border-b">
-                    <p>Signature:</p>
-                </div>
-            </div>
-
-            <!-- Print Button -->
-            <div class="mt-6 text-center">
-                <button
-                    class="bg-blue-500 text-white px-4 py-2 rounded shadow"
-                    @click="printReceipt(customerName)"
-                >
-                    Print Receipt
-                </button>
-            </div>
-        </div>
-    </div>
+    <PosViewReceipt
+        :id="cartStore.id"
+        :cash-tendered="cashTendered"
+        :change="change"
+        :customer-guest="customerName"
+        :date="transactionDate"
+        :order-items="cartStore.cartItems"
+        :promotion-amount="cartStore.promotionAmount"
+        :sub-total="cartStore.totalAmount"
+        :total-amount="cartStore.totalAmountWithTaxAndDiscount"
+        :total-tax="cartStore.totalTax"
+        :transaction-number="generateTransactionId"
+    />
 </template>
 
 <script setup lang="ts">
-const auth = useAuth();
 const cartStore: any = useCart();
 const customerName: any = inject('customerName');
-const customerAddress = ref('');
-const customerType = ref('Guest');
 
 const transactionDate = toBasicDateTime(new Date());
 
@@ -174,14 +29,3 @@ const change: ComputedRef<number> = computed(() =>
     ),
 );
 </script>
-
-<style scoped>
-@media print {
-    button {
-        display: none;
-    }
-}
-* {
-    font-family: 'Courier New', Courier, monospace;
-}
-</style>
