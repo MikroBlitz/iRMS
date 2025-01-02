811c972d6e4920ba7b4ba5459513965547c2a405
MkTz <94935785+maktezi@users.noreply.github.com>
2025-01-02 13:16:26 +0800

    refactor: landing page (#155)

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -7,14 +7,7 @@
         </Head>
 
         <div v-if="showContent" class="absolute top-6 left-6">
-            <div class="flex items-center justify-center gap-2">
-                <DisplayMode />
-                <NuxtLink :to="auth.user.id ? '/dashboard' : '/login'">
-                    <Button class="w-[100px] bg-primary dark:bg-secondary">
-                        {{ auth.user.id ? 'Dashboard' : 'Login' }}
-                    </Button>
-                </NuxtLink>
-            </div>
+            <DisplayMode />
         </div>
 
         <div
@@ -22,20 +15,68 @@
         >
             <div
                 v-if="showContent"
-                class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 h-[75vh] w-[90vw] mx-auto"
+                class="flex flex-col items-center justify-center max-w-[1200px] p-6 w-full h-auto"
             >
-                <div
-                    v-for="item in items"
-                    class="feature-section bg-cover bg-center hover:scale-105 transition-transform duration-300"
-                    :style="{ backgroundImage: `url(${item.image})` }"
-                >
-                    <div
-                        class="bg-black bg-opacity-90 p-5 text-center text-white"
-                    >
-                        <h1 class="text-xl lg:text-2xl">
-                            {{ item.title }}
+                <div>
+                    <div>
+                        <NuxtImage
+                            src="/landing/application_logo.png"
+                            class="w-32"
+                        />
+                    </div>
+
+                    <div>
+                        <link
+                            href="https://fonts.googleapis.com/css2?family=Poppins:wght@800&display=swap"
+                            rel="stylesheet"
+                        />
+                        <h1
+                            class="text-md md:text-[3em] leading-tight extrabold-font text-center mb-4 bg-gradient-to-b from-emerald-500 to-indigo-500 text-transparent bg-clip-text"
+                        >
+                            Empower your business with<br />
+                            an All-in-One system
                         </h1>
                     </div>
+
+                    <video
+                        autoplay
+                        loop
+                        muted
+                        class="rounded-md md:rounded-xl border-2 border-secondary shadow-md shadow-gray-500 dark:shadow-black"
+                    >
+                        <source
+                            src="/landing/landing-video.mp4"
+                            type="video/mp4"
+                        />
+                        Your browser does not support the video tag.
+                    </video>
+                </div>
+
+                <div
+                    class="md:flex md:space-x-4 space-y-2 md:space-y-0 items-center justify-center mt-8 w-full"
+                >
+                    <NuxtLink :to="auth.user.id ? '/dashboard' : '/login'">
+                        <Button
+                            class="shadow-md shadow-gray-500 dark:shadow-primary bg-emerald-700 font-bold w-full md:w-60 py-6 rounded-xl transition duration-300 hover:bg-emerald-500"
+                        >
+                            {{ auth.user.id ? 'Dashboard' : 'Login' }}
+                        </Button>
+                    </NuxtLink>
+                    <Button
+                        :disabled="demoLoading"
+                        class="shadow-md shadow-gray-500 dark:shadow-primary bg-indigo-800 w-full md:w-60 py-6 rounded-xl transition duration-300 hover:bg-indigo-500"
+                        @click.prevent="demo"
+                    >
+                        <SpinnerTadpole
+                            :class="{ hidden: !demoLoading }"
+                            class="size-7 text-card dark:text-card-foreground mx-1"
+                        />
+                        <span
+                            class="font-bold"
+                            :class="{ 'animate-pulse ml-2': demoLoading }"
+                            >{{ demoLoading ? 'Loading...' : 'Try Demo' }}</span
+                        >
+                    </Button>
                 </div>
             </div>
 
@@ -52,33 +93,26 @@ import { Button } from '~/components/ui/button';
 
 const auth = useAuth();
 const showContent = ref(false);
+const demoLoading = ref(false);
 
-onMounted(() => {
-    useTimeoutFn(() => {
-        showContent.value = true;
-    }, 2000);
-});
-
-const items = [
-    {
-        image: 'https://i.imgur.com/UULsFJV.png',
-        title: 'Fast and Reliable',
-    },
-    {
-        image: 'https://i.imgur.com/1tZB8Qw.png',
-        title: 'Easy-to-use Interface',
-    },
-    {
-        image: 'https://i.imgur.com/mCSSn0K.png',
-        title: 'Secure and Encrypted',
-    },
-    {
-        image: 'https://i.imgur.com/nJ2qOhw.png',
-        title: '24/7 Customer Support',
-    },
-];
+onMounted(() => useTimeoutFn(() => (showContent.value = true), 2000));
 
 definePageMeta({
     layout: false,
 });
+
+const demo = () => {
+    demoLoading.value = true;
+    useTimeoutFn(() => {
+        demoLoading.value = false;
+        navigateTo('/dashboard');
+    }, 3000);
+};
 </script>
+
+<style scoped>
+.extrabold-font {
+    font-family: 'Poppins';
+    font-weight: 700;
+}
+</style>
--- web/stores/useAuth.ts
+++ web/stores/useAuth.ts
@@ -6,19 +6,19 @@ import axios from '~/plugins/axios';
 const $axios = axios().provide.axios;
 
 export const useAuth = defineStore('auth', {
-    state: () => ({
-        user: {
-            id: '',
-            role: null,
-            first_name: '',
-            middle_name: '',
-            last_name: '',
-            name: '',
-        },
-    }),
     actions: {
         async getTokens() {
             await $axios.get('/sanctum/csrf-cookie');
+        },
+        async getUser() {
+            const response = await $axios.get('/api/authenticated-user');
+
+            this.$state.user.id = response.data[0].id;
+            this.$state.user.role = response.data[0].role;
+            this.$state.user.first_name = response.data[0].first_name;
+            this.$state.user.middle_name = response.data[0].middle_name;
+            this.$state.user.last_name = response.data[0].last_name;
+            this.$state.user.name = response.data[0].name;
         },
         async login(email: string, password: string) {
             try {
@@ -39,16 +39,6 @@ export const useAuth = defineStore('auth', {
                 await mutate({ input: log });
             }
         },
-        async getUser() {
-            const response = await $axios.get('/api/authenticated-user');
-
-            this.$state.user.id = response.data[0].id;
-            this.$state.user.role = response.data[0].role;
-            this.$state.user.first_name = response.data[0].first_name;
-            this.$state.user.middle_name = response.data[0].middle_name;
-            this.$state.user.last_name = response.data[0].last_name;
-            this.$state.user.name = response.data[0].name;
-        },
         async logout() {
             try {
                 const response = await $axios.get('/api/authenticated-user');
@@ -63,7 +53,7 @@ export const useAuth = defineStore('auth', {
                 console.error('Logout failed:', error);
             } finally {
                 this.resetUser();
-                navigateTo('/login');
+                navigateTo('/');
             }
         },
         resetUser() {
@@ -76,4 +66,14 @@ export const useAuth = defineStore('auth', {
         },
     },
     persist: true,
+    state: () => ({
+        user: {
+            first_name: '',
+            id: '',
+            last_name: '',
+            middle_name: '',
+            name: '',
+            role: null,
+        },
+    }),
 });
