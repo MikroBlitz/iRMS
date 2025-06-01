476d9314e940c097403c6a68bda3ac87bcfe04e2
MkTz <94935785+maktezi@users.noreply.github.com>
2025-06-01 22:03:39 +0800

    fix: deployment (#8)

--- web/plugins/axios.ts
+++ web/plugins/axios.ts
@@ -1,13 +1,14 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export default defineNuxtPlugin(() => {
-    axios.defaults.withCredentials = true
-    axios.defaults.baseURL = 'http://localhost:8000'
-    // axios.defaults.baseURL = useRuntimeConfig().public.API_URL;
+	axios.defaults.withCredentials = true;
+	axios.defaults.baseURL = 'https://pos.markethub.blog';
+	// axios.defaults.baseURL = 'http://localhost:8000'
+	// axios.defaults.baseURL = useRuntimeConfig().public.API_URL;
 
-    return {
-        provide: {
-            axios: axios,
-        },
-    }
-})
+	return {
+		provide: {
+			axios: axios,
+		},
+	};
+});
